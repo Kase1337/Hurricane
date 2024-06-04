8b264007a298e05a046a9bc0122c957eea577568
Kase1337 <linkinusf1@gmail.com>
2024-06-04 23:43:16 +0000

    Updated the code to allow 'mph' to be entered with the speed value. Should fix the error that the previous code would had.

--- main.cs
+++ main.cs
@@ -4,18 +4,27 @@ class Program {
   public static void Main (string[] args) {
     //Category information taken from "Saffir-Simpson Hurricane Scale" link: https://www.weather.gov/mfl/saffirsimpson#:~:text=The%20Saffir%2DSimpson%20Hurricane%20Wind,loss%20of%20life%20and%20damage. 
     
-    //speed and category variales
-    int windspeed;
+    //speed and category and input variales
+    double windspeed;
     string category;
+    string input;
+
+   
     
     // Prompt the user to enter the wind speed
         Console.Write("Please enter the wind speed in miles per hour: ");
 
     //Read the windspeed entered from by user and convert the input to an integer
-        windspeed = Convert.ToInt32(Console.ReadLine()); 
+      //  windspeed = Convert.ToDouble(Console.ReadLine()); 
+
+    //read the entered 
+  input = Console.ReadLine();
     
-    // Compare and determinate the hurricane category based on the wind category entered by the user
-    //Use if statement to determine the windspeed category
+    // Remove mph from the input string and then parse the information to double to determinate the hurricane category based on the wind category entered by the user
+
+    
+      if (double.TryParse(input.Replace(" mph", ""), out windspeed))
+      {
         if (windspeed >= 157)
         {
             category = "Category Five Hurricane"; // Category 5
@@ -45,7 +54,12 @@ class Program {
          Console.WriteLine(category);
     }
 
+    else // If the input is not a valid number, print an error message.
+    {
+        Console.WriteLine("Invalid input. Please enter the wind speed in the format 'value mph'.");
+    }
 
-    
+
+    }
+  }
   
-}
